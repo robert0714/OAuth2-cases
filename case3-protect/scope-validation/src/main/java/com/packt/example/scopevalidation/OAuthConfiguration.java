caa6c6f9cadcaff9c56b85cc6c1cf7f04304044d
robert.lee <robert0714@gmail.com>
2018-11-18 17:14:14 +0800

    add senario

---
+++ case3-protect/scope-validation/src/main/java/com/packt/example/scopevalidation/OAuthConfiguration.java
@@ -0,0 +1,36 @@
+package com.packt.example.scopevalidation;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+public class OAuthConfiguration {
+
+    @EnableAuthorizationServer
+    public static class AuthorizationServer
+        extends AuthorizationServerConfigurerAdapter {
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+                .withClient("clientapp").secret("123")
+                .scopes("read_x", "read_y")
+                .authorizedGrantTypes("authorization_code")
+                .redirectUris("http://localhost:9000/callback")//spring-security-oauth2 2.3.4.RELEASE與2.2.0.RELEASE的最大差異
+                ;
+        }
+    }
+
+    @EnableResourceServer
+    public static class ResourceServer
+        extends ResourceServerConfigurerAdapter {
+        public void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().authenticated().and()
+                .requestMatchers().antMatchers("/api/**");
+        }
+    }
+
+}
